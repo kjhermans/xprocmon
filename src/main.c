14b9df71ef7da42b92bedb8e35ab3079152c6080
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2025-04-04 17:38:17 +0200

    Generic skeleton for the functional intention of this executable: fork to run the executable, then monitor the executable, then wait for its signals.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <errno.h>
 
+#include "xprocmon.h"
+
+extern char** environ;
+
+static
+void sighandler
+  (int signum)
+{
+  fprintf(stderr, "Signal trapped %d\n", signum);
+//..
+}
 
 int main
   (int argc, char* argv[])
 {
-  xprocmon_window();
+  pid_t pid = 0;
+
+  if (argc < 2) {
+    fprintf(stderr, "Argument to run must be provided.\n");
+    exit(-1);
+  }
+  signal(SIGINT, sighandler);
+  switch (pid = fork()) {
+  case -1:
+    perror("fork");
+    exit(-2);
+  case 0:
+    if (execve(argv[ 1 ], &(argv[ 1 ]), environ)) {
+      exit(errno);
+    }
+    exit(0);
+  default:
+    break;
+  }
+  xprocmon_window(pid);
   return 0;
 }
