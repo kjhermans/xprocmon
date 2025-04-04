e97bf7077b73963d9d46603566cee0dce0f512bd
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2025-04-05 00:55:12 +0200

    Removed the example code crud and started replacing it with something that reflects more of the real data I'm collecting.

--- src/xprocmon_window.c
+++ src/xprocmon_window.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#include <sys/utsname.h>
-
 #include <X11/X.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
@@ -31,9 +29,6 @@ int xprocmon_window
   Atom WM_DELETE_WINDOW = XInternAtom(dpy, "WM_DELETE_WINDOW", False); 
   XSetWMProtocols(dpy, win, &WM_DELETE_WINDOW, 1);  
 
-  struct utsname sname;
-  uname(&sname);
-
   XEvent e;
   while (1) 
   {
@@ -41,39 +36,28 @@ int xprocmon_window
     int r = XNextEventTimed(dpy, &e, &tv);
     xprocmon_t pm = { 0 };
 
-    xprocmon_stat(pid, &pm);
+    xprocmon_maps(pid, &pm);
     if (r == False || e.type == Expose) 
     {
       int y_offset = 20;
  
-      const char* s1 = "X11 test app under Linux";
+      const char* s1 = "XProcMon";
       
-      const char* s2 = "(C)2012 Geeks3D.com"; 
       XDrawString(dpy, win, DefaultGC(dpy, s), 10, y_offset, s1, strlen(s1));
-      y_offset += 20;
-      XDrawString(dpy, win, DefaultGC(dpy, s), 10, y_offset, s2, strlen(s2));
       y_offset += 20;
 
       {
         char buf[256] = {0};
         
-        sprintf(buf, "System information:");
+        sprintf(buf, "- # Threads: %u", pm.numthreads);
         XDrawString(dpy, win, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
         y_offset += 15;
 
-        sprintf(buf, "- System: %s", sname.sysname);
-        XDrawString(dpy, win, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
-        y_offset += 15;
-
-        sprintf(buf, "- Release: %s", sname.release);
-        XDrawString(dpy, win, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
-        y_offset += 15;
-
-        sprintf(buf, "- Version: %s", sname.version);
+        sprintf(buf, "- Stack: %u", pm.mem.stack);
         XDrawString(dpy, win, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
         y_offset += 15;
         
-        sprintf(buf, "- Machine: %s", sname.machine);
+        sprintf(buf, "- Heap: %u", pm.mem.heap);
         XDrawString(dpy, win, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
         y_offset += 20;
       }
@@ -94,11 +78,14 @@ int xprocmon_window
       char buf[128] = {0};
       KeySym keysym;
       int len = XLookupString(&e.xkey, buf, sizeof buf, &keysym, NULL);
-      if (keysym == XK_Escape)
+      if (keysym == XK_Escape) {
         break;
+      }
+      (void)len;
     }
 
-    if ((e.type == ClientMessage) && (e.xclient.data.l[0] == WM_DELETE_WINDOW))
+    if ((e.type == ClientMessage) &&
+        ((Atom)(e.xclient.data.l[0]) == WM_DELETE_WINDOW))
     {
       break;
     }
