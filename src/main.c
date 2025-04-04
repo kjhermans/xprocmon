9ca73e15923bb15adac9c30113246a1762927fcd
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2025-04-05 00:54:03 +0200

    Rough code wrt child handling.

--- src/main.c
+++ src/main.c
@@ -3,28 +3,36 @@
 #include <signal.h>
 #include <errno.h>
 
+#include <sys/wait.h>
+
 #include "xprocmon.h"
 
 extern char** environ;
+static pid_t pid = 0;
 
 static
 void sighandler
   (int signum)
 {
   fprintf(stderr, "Signal trapped %d\n", signum);
-//..
+  switch (signum) {
+  case SIGINT:
+  case SIGTERM:
+  case SIGCHLD:
+    { int status = 0; waitpid(pid, &status, 0); exit(status); }
+  }
 }
 
 int main
   (int argc, char* argv[])
 {
-  pid_t pid = 0;
-
   if (argc < 2) {
     fprintf(stderr, "Argument to run must be provided.\n");
     exit(-1);
   }
   signal(SIGINT, sighandler);
+  signal(SIGTERM, sighandler);
+  signal(SIGCHLD, sighandler);
   switch (pid = fork()) {
   case -1:
     perror("fork");
