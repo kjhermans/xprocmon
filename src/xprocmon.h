4f8c9693cbce8e3a8c0e6a2e3699bb21ebd291ac
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2025-04-05 00:56:38 +0200

    Function prototypes are more and more important esp if gcc assumes a different size for an int (default return type for an unknown function), and a pointer.

--- src/xprocmon.h
+++ src/xprocmon.h
@@ -19,4 +19,20 @@ extern
 int xprocmon_window
   (pid_t pid);
 
+extern
+int xprocmon_stat
+  (pid_t pid, xprocmon_t* pm);
+
+extern
+int xprocmon_maps
+  (pid_t pid, xprocmon_t* pm);
+
+extern
+int absorb_file
+  (char* path, unsigned char** buf, unsigned* buflen);
+
+extern
+char* dup_file_string
+  (char* path);
+
 #endif
