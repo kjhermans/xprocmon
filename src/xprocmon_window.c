864a705e044e274d44047435bb923497fb398a0c
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2025-04-06 10:35:25 +0200

    Lots of (irrelevant, for the final product) changes. Just getting to know X11 again.

--- src/xprocmon_window.c
+++ src/xprocmon_window.c
@@ -36,9 +36,19 @@ int xprocmon_window
     int r = XNextEventTimed(dpy, &e, &tv);
     xprocmon_t pm = { 0 };
 
-    xprocmon_maps(pid, &pm);
+    int d = xprocmon_data(pid, &pm); (void)d;
+
     if (r == False || e.type == Expose) 
     {
+      XWindowAttributes  wa;
+      XGetWindowAttributes(dpy, win, &wa);
+      int width = wa.width;
+      int height = wa.height;
+
+      XSetForeground(dpy, DefaultGC(dpy, s), WhitePixel(dpy, s));
+      XFillRectangle(dpy, win, DefaultGC(dpy, s), 0, 0, width, height);
+      XSetForeground(dpy, DefaultGC(dpy, s), BlackPixel(dpy, s));
+
       int y_offset = 20;
  
       const char* s1 = "XProcMon";
@@ -46,28 +56,32 @@ int xprocmon_window
       XDrawString(dpy, win, DefaultGC(dpy, s), 10, y_offset, s1, strlen(s1));
       y_offset += 20;
 
-      {
-        char buf[256] = {0};
+      char buf[256] = {0};
         
-        sprintf(buf, "- # Threads: %u", pm.numthreads);
-        XDrawString(dpy, win, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
-        y_offset += 15;
+      sprintf(buf, "- CSTime: %ld", pm.cstime);
+      XDrawString(dpy, win, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
+      y_offset += 15;
 
-        sprintf(buf, "- Stack: %u", pm.mem.stack);
-        XDrawString(dpy, win, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
-        y_offset += 15;
+      sprintf(buf, "- STime: %ld", pm.stime);
+      XDrawString(dpy, win, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
+      y_offset += 15;
+
+      sprintf(buf, "- # Threads: %u", pm.numthreads);
+      XDrawString(dpy, win, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
+      y_offset += 15;
+
+      sprintf(buf, "- Stack: %u", pm.mem.stack);
+      XDrawString(dpy, win, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
+      y_offset += 15;
         
-        sprintf(buf, "- Heap: %u", pm.mem.heap);
-        XDrawString(dpy, win, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
-        y_offset += 20;
-      }
+      sprintf(buf, "- Heap: %u", pm.mem.heap);
+      XDrawString(dpy, win, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
+      y_offset += 15;
 
+      sprintf(buf, "- Total: %u", pm.mem.total);
+      XDrawString(dpy, win, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
+      y_offset += 20;
 
-      XWindowAttributes  wa;
-      XGetWindowAttributes(dpy, win, &wa);
-      int width = wa.width;
-      int height = wa.height;
-      char buf[128]={0};
       sprintf(buf, "Current window size: %dx%d", width, height);
       XDrawString(dpy, win, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
       y_offset += 20;
